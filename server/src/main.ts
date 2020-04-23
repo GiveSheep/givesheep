69c10935b78f50b2328821956b5420a0e282248e
songyang <songyang@ict.ac.cn>
2020-04-23 15:35:06 +0800

    添加跨域

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 处理跨域
+  app.enableCors();
+
   const options = new DocumentBuilder()
     .setTitle('nestjs练习')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
